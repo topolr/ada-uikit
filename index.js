d941675e0a38fb765939e22236c6fd4ca5a5affb
hou80houzhu <hou80houzhu@vip.qq.com>
2017-12-19 15:24:57 +0800

    pass

--- index.js
+++ index.js
@@ -5,5 +5,28 @@ import Photocutter from "./src/photocutter";
 import Router from "./src/router";
 import {Sprite, Scene} from "./src/displayer";
 import Tween from "./src/tween";
+import transform from "./src/transform";
+import transition from "./src/transition";
+import query from "./src/query";
+import observe from "./src/observe";
+import is from "./src/is";
+import browser from "./src/browser";
+import util from "./src/util";
 
-export {Loading, Toast, File, Photocutter, Router, Sprite, Scene, Tween};
+export {
+    Loading,
+    Toast,
+    File,
+    Photocutter,
+    Router,
+    Sprite,
+    Scene,
+    Tween,
+    transform,
+    transition,
+    query,
+    observe,
+    is,
+    browser,
+    util
+};
