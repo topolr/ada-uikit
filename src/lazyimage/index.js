419e6716dee286d068a06509ef77eb4c8ce06e8f
Jinliang <hou80houzhu@vip.qq.com>
2018-06-28 23:39:19 +0800

    pass

--- src/lazyimage/index.js
+++ src/lazyimage/index.js
@@ -4,41 +4,42 @@ import eventDispatcher from "../dispatcher";
 import "./../style/base.scss";
 
 @view({
-	className: "lazyimage",
-	template: "./template.html",
-	style: "./style.scss",
-	dataset: {
-		service: LazyimageService
-	}
+    className: "lazyimage",
+    template: "./template.html",
+    style: "./style.scss",
+    dataset: {
+        service: LazyimageService
+    }
 })
 class Lazyimage extends View {
-	oncreated() {
-		eventDispatcher.observe(this);
-		this.scroll();
-	}
+    oncreated() {
+        eventDispatcher.observe(this);
+        this.scroll();
+    }
 
-	@subscribe("scroll")
-	scroll() {
-		let height = document.documentElement.clientHeight;
-		let top = this.getElement().getBoundingClientRect().top;
-		if (top <= height) {
-			this.loadImage();
-		}
-	}
+    @subscribe("scroll")
+    scroll() {
+        let height = document.documentElement.clientHeight;
+        let top = this.getElement().getBoundingClientRect().top;
+        if (top <= height) {
+            this.loadImage();
+        }
+    }
 
-	loadImage() {
-		let state = this.getCurrentState();
-		if (state.url && !state.loaded) {
-			let image = document.createElement("img");
-			image.addEventListener("load", () => {
-				let target = this.finder("image").getElement();
-				target.innerHTML = "";
-				target.appendChild(image);
-				this.commit("loaded", true);
-			});
-			image.setAttribute("src", state.url);
-		}
-	}
+    loadImage() {
+        let state = this.getCurrentState();
+        if (state.url && !state.loaded) {
+            let image = document.createElement("img");
+            image.addEventListener("load", () => {
+                let target = this.finder("image").getElement();
+                target.innerHTML = "";
+                target.appendChild(image);
+                eventDispatcher.unobserve(this);
+                this.commit("loaded", true);
+            });
+            image.setAttribute("src", state.url);
+        }
+    }
 }
 
 export default Lazyimage;
