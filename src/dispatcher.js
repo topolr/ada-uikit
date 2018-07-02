f33c4e90902e87cf4deae141d5bdbe38680ff3c8
Jinliang <hou80houzhu@vip.qq.com>
2018-07-02 20:49:34 +0800

    pass

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,15 +1,17 @@
 import {Dispatcher} from "adajs";
 
 class EventDispatcher extends Dispatcher {
-	constructor() {
-		super();
-		window.document.body.addEventListener("scroll", e => {
-			this.dispatch("scroll", e);
-		});
-		window.document.body.addEventListener("click", e => {
-			this.dispatch("click", e);
-		});
-	}
+    constructor() {
+        super();
+        window.document.body.addEventListener("scroll", e => {
+            window.requestAnimationFrame(() => {
+                this.dispatch("scroll", e);
+            });
+        });
+        window.document.body.addEventListener("click", e => {
+            this.dispatch("click", e);
+        });
+    }
 }
 
 const eventDispatcher = new EventDispatcher();
