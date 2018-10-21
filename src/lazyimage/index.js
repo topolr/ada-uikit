2af1664633abb56471f3b3d0e5f4f1eb73bbdb68
Jinliang <hou80houzhu@vip.qq.com>
2018-10-21 21:13:27 +0800

    pass

--- src/lazyimage/index.js
+++ src/lazyimage/index.js
@@ -4,50 +4,55 @@ import eventDispatcher from "../dispatcher";
 import "./../style/base.scss";
 
 @view({
-	className: "lazyimage",
-	template: "./template.html",
-	style: "./style.scss",
-	dataset: {
-		service: LazyimageService
-	}
+    className: "lazyimage",
+    template: "./template.html",
+    style: "./style.scss",
+    dataset: {
+        service: LazyimageService
+    }
 })
 class Lazyimage extends View {
-	oncreated() {
-		if (this.context.isBrowser) {
-			eventDispatcher.observe(this);
-			setTimeout(() => this.scroll());
-		}
-	}
+    oncreated() {
+        if (this.context.isBrowser) {
+            eventDispatcher.observe(this);
+            setTimeout(() => this.scroll());
+        }
+    }
 
-	onready() {
-		if (!this.context.isBrowser) {
-			this.finder("image").getElement().innerHTML = `<img src="${this.getCurrentState().url}"/>`;
-		}
-	}
+    onready() {
+        if (!this.context.isBrowser) {
+            this.finder("image").getElement().innerHTML = `<img src="${this.getCurrentState().url}"/>`;
+        }
+    }
 
-	@subscribe("scroll")
-	scroll() {
-		let height = document.documentElement.clientHeight;
-		let top = this.getElement().getBoundingClientRect().top;
-		if (top <= height) {
-			this.loadImage();
-		}
-	}
+    @subscribe("scroll")
+    scroll() {
+        let height = document.documentElement.clientHeight;
+        let top = this.getElement().getBoundingClientRect().top;
+        if (top <= height) {
+            this.loadImage();
+        }
+    }
 
-	loadImage() {
-		let state = this.getCurrentState();
-		if (state.url && !state.loaded) {
-			let image = document.createElement("img");
-			image.addEventListener("load", () => {
-				let target = this.finder("image").getElement();
-				target.innerHTML = "";
-				target.appendChild(image);
-				eventDispatcher.unobserve(this);
-				this.commit("loaded", true);
-			});
-			image.setAttribute("src", state.url);
-		}
-	}
+    loadImage() {
+        let state = this.getCurrentState();
+        if (state.url && !state.loaded) {
+            let image = document.createElement("img");
+            image.addEventListener("load", () => {
+                let target = this.finder("image").getElement();
+                target.innerHTML = "";
+                target.appendChild(image);
+                eventDispatcher.unobserve(this);
+                this.commit("loaded", true);
+            });
+            image.setAttribute("src", state.url);
+        }
+    }
+
+    onrecover() {
+        eventDispatcher.observe(this);
+        setTimeout(() => this.scroll());
+    }
 }
 
 export default Lazyimage;
